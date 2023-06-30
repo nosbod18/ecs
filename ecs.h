618ef8940ff7b6a97fe263eddf77c229af699d36
Evan Dobson <evan.dobson18@gmail.com>
2023-06-30 09:52:50 -0500

    Updated interface

--- ecs.h
+++ ecs.h
@@ -4,14 +4,20 @@
 
 typedef uint64_t ecs_id_t;
 
-void        ecs_init                (void);
-ecs_id_t    ecs_create              (void);
-#define     ecs_set(T, entity, ...) _ecs_set(#T, (entity), sizeof (T), (__VA_ARGS_))
-#define     ecs_get(T, entity)      _ecs_get(#T, (entity))
-#define     ecs_rem(T, entity)      _ecs_rem(#T, (entity))
-void        ecs_delete              (ecs_id_t id);
-void        ecs_quit                (void);
+typedef struct ecs_world_t ecs_world_t;
 
-void      *_ecs_set                 (char const *component, ecs_id_t entity, size_t stride, void const *data);
-void      *_ecs_get                 (char const *component, ecs_id_t entity);
-void      *_ecs_rem                 (char const *component, ecs_id_t entity);
+typedef struct {
+    void *pool, *pools[16];
+    int index_to_com[16];
+    int count;
+    size_t entity_index;
+    uint64_t entity;
+} ecs_view_t;
+
+ecs_world_t    *ecs_new     (int component_count, ...);
+ecs_id_t        ecs_spawn   (ecs_world_t *world);
+void            ecs_set     (ecs_world_t *world, ecs_id_t entity, int component, void const *data);
+void           *ecs_get     (ecs_world_t *world, ecs_id_t entity, int component);
+void            ecs_rem     (ecs_world_t *world, ecs_id_t entity, int component);
+void            ecs_despawn (ecs_world_t *world, ecs_id_t entity);
+void            ecs_free    (ecs_world_t *world);
